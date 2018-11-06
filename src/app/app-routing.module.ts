0c6e8b97b787471245df14442abe6b4a09cef4ae
Javier Salvador Ruiz Samano <ruizinova@gmail.com>
2018-11-05 20:57:38 -0600

    Se agrega al nav la ruta del componente del formulario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { MyTableComponent } from './my-table/my-table.component';
 import { MyDashComponent } from './my-dash/my-dash.component';
 import { MyTreeComponent } from './my-tree/my-tree.component';
 import { MyDdComponent } from './my-dd/my-dd.component';
+import { MyFormComponent } from './my-form/my-form.component';
 
 const routes: Routes = [
   { path: '', component: MyDashComponent },
   { path: 'personlist', component: MyTableComponent },
+  { path: 'addperson', component: MyFormComponent },
   { path: 'arbol', component: MyTreeComponent },
   { path: 'drag-drop', component: MyDdComponent },
   { path: '**', redirectTo: '' }
